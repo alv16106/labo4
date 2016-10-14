ba8c9d3d58951105d07a9e68feadfc18279140ff
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2016-10-14 09:05:39 -0600

    Add files via upload
    Subiendo la AlarmaEmergencia

---
+++ AlarmaEmergencia.java
@@ -0,0 +1,12 @@
+public class AlarmaEmergencia extends Alarma {
+	public void m1() {
+		System.out.println("Emergencia1");
+	}
+	public void m2() {
+		super.m1();
+		super.m2();
+	}
+	public String toString() {
+		return "Emergencia "+super.toString();
+	}
+}
